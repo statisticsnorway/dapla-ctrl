1adfb6383173e3daa7ad732b28264f4119b71aea
johnnadeluy <johnnadel.salita@gmail.com>
2024-01-16 14:56:35 +0100

    Update table props
    DPSTAT-801

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,19 +1,41 @@
 import styles from './table.module.scss'
 
-export default function Table () {
+export interface TableData {
+    columns: {
+        id: string,
+        label: string,
+    }[],
+    data: {
+        id: string,
+        [key: string]: React.ReactNode
+    }[]
+}
+
+export default function Table ({columns, data}: TableData) {
   return (
       <table className={styles.table}>
         <thead>
           <tr>
-              <th>Lorem ipsum</th>
+            {columns.map((column) => (
+                <th key={column.id}>
+                    {column.label}
+                </th>
+            ))}
           </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>
-                  Lorem ipsum
-                </td>
-            </tr>
+            {data.map((row, index) => {
+                // Add conditional styling for first element, then third etc
+                const conditionalStyling = (index + 1) % 2 !== 0 ? styles.conditionalCell : undefined
+                return (
+                    <tr key={row.id} className={conditionalStyling}>
+                        {columns.map((column) => (
+                            <td key={column.id}>
+                                {row[column.id]}
+                            </td>
+                        ))}
+                    </tr>)
+            })}
         </tbody>
       </table>
   )
