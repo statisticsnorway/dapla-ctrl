4a05f87d047c1456a21f1e4b1cbd15d33283d65b
ssb-jnk <jnk@ssb.no>
2024-01-23 15:53:30 +0100

    .

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -10,7 +10,9 @@ interface PageLayoutProps {
 
 export default function Avatar({ fullName }: PageLayoutProps) {
     const [userProfileData, setUserProfileData] = useState<User>();
-    const [imageSrc, setImageSrc] = useState<string>("");
+    const [imageSrc, setImageSrc] = useState<string>();
+
+    const fallbackInitials = `${fullName.split(' ')[0][0]}${fullName.split(' ')[1][0]}`
 
     const navigate = useNavigate();
     const encodedURI = encodeURI(`/teammedlemmer/${fullName}`);
@@ -23,8 +25,11 @@ export default function Avatar({ fullName }: PageLayoutProps) {
         }
 
         const userProfile = JSON.parse(storedUserProfile) as User;
-        const base64Image = userProfile?.photo;
+        if (!userProfile) return;
         setUserProfileData(userProfile);
+
+        const base64Image = userProfile?.photo;
+        if (!base64Image) return;
         setImageSrc(`data:image/png;base64,${base64Image}`);
     }, []);
 
@@ -38,7 +43,7 @@ export default function Avatar({ fullName }: PageLayoutProps) {
                 <img src={imageSrc} alt="User" />
             ) : (
                 <div className={styles.initials}>
-                    {userProfileData ? `${userProfileData.firstName?.[0]}${userProfileData.lastName?.[0]}` : '??'}
+                    {userProfileData ? `${fallbackInitials}` : '??'}
                 </div>
             )}
         </div>
