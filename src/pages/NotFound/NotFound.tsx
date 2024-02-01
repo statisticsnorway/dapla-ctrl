f8f44dfea5d1fd1f918603a302357be8eb3b0bac
sigurdrj <sigurdrj@protonmail.com>
2024-02-01 14:19:32 +0100

    Run lint:format

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -5,8 +5,12 @@ import { Title } from '@statisticsnorway/ssb-component-library'
 export default function NotFound() {
   return (
     <div className={notFoundStyles.CenterDiv}>
-        <Title size={1} className={notFoundStyles.Title}>Page not found!</Title>
-        <Title size={2} className={notFoundStyles.SubTitle}>Error code: 404</Title>
+      <Title size={1} className={notFoundStyles.Title}>
+        Page not found!
+      </Title>
+      <Title size={2} className={notFoundStyles.SubTitle}>
+        Error code: 404
+      </Title>
     </div>
   )
 }
