0c805bd09f3801c179dd26890c7aa5a87904ac34
sigurdrj <sigurdrj@protonmail.com>
2024-02-01 10:29:55 +0100

    Add styling to NotFound

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -3,5 +3,10 @@ import notFoundStyles from './notFound.module.scss'
 import { Title } from '@statisticsnorway/ssb-component-library'
 
 export default function NotFound() {
-  return <Title size={1}>404 Not Found</Title>
+  return (
+    <>
+      <Title size={1} className={notFoundStyles.Title}>404</Title>
+      <Title size={2} className={notFoundStyles.SubTitle}>Not Found</Title>
+    </>
+  )
 }
