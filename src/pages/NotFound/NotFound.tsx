401525797cd83c931f9192580f19ad7668b74c34
sigurdrj <sigurdrj@protonmail.com>
2024-02-01 11:50:08 +0100

    Center everything

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -4,9 +4,9 @@ import { Title } from '@statisticsnorway/ssb-component-library'
 
 export default function NotFound() {
   return (
-    <>
-      <Title size={1} className={notFoundStyles.Title}>404</Title>
-      <Title size={2} className={notFoundStyles.SubTitle}>Not Found</Title>
-    </>
+    <div className={notFoundStyles.CenterDiv}>
+        <Title size={1} className={notFoundStyles.Title}>Page not found!</Title>
+        <Title size={2} className={notFoundStyles.SubTitle}>Error code: 404</Title>
+    </div>
   )
 }
