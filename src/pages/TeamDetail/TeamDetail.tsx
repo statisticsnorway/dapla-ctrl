d725b81d35f431db0590f8e606b6ea3991b848e5
Johnnadel Salita Uy <johnnadel.salita@gmail.com>
2024-02-02 11:38:52 +0100

    Part 3: Add workflow build and lint (#26)
    * Adds a workflow that builds and lints on open pull-requests to develop
    
    * Include auto fix for linting in workflow
    
    * Minor code refactoring and adjust Prettier warnings to error
    
    * Fix type error for breadcrumbsUserProfileDisplayName
    
    * Temporary hide unused const in TeamDetail to prevent build fail
    
    * Adjustments to not include autofix ESLint and Prettier in workflow
    
    * Update README.md with ESLint and Prettier documentation with ChatGPT assistance
    
    * Autofix prettier format errors
    
    * Bump actions/checkout@v3 to actions/checkout@v4
    
    * Minor code refactoring; rewording

--- src/pages/TeamDetail/TeamDetail.tsx
+++ src/pages/TeamDetail/TeamDetail.tsx
@@ -1,37 +1,38 @@
-
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react'
 import PageLayout from '../../components/PageLayout/PageLayout'
 import { TeamDetailData, getTeamDetail } from '../../api/teamDetail'
 import { useParams } from 'react-router-dom'
 import { ErrorResponse } from '../../@types/error'
 
-
 export default function TeamOverview() {
-    const { teamId } = useParams<{ teamId: string }>()
-    const [teamDetailData, setTeamDetailData] = useState<TeamDetailData>()
-    const [error, setError] = useState<ErrorResponse | undefined>()
-    const [loading, setLoading] = useState<boolean>(true)
+  const { teamId } = useParams<{ teamId: string }>()
+  // TODO: Temporary hide these so build doesn't fail
+  // const [teamDetailData, setTeamDetailData] = useState<TeamDetailData>()
+  // const [error, setError] = useState<ErrorResponse | undefined>()
+  // const [loading, setLoading] = useState<boolean>(true)
 
-    useEffect(() => {
-        if (!teamId) return
-        console.log(teamId)
-        getTeamDetail(teamId)
-            .then((response) => {
-                if ((response as ErrorResponse).error) {
-                    console.log(response)
-                    setError(response as ErrorResponse)
-                } else {
-                    console.log((response))
-                    setTeamDetailData(response as TeamDetailData)
-                }
-            })
-            .finally(() => setLoading(false))
-            .catch((error) => {
-                setError(error.toString())
-            })
+  const [, setTeamDetailData] = useState<TeamDetailData>()
+  const [, setError] = useState<ErrorResponse | undefined>()
+  const [, setLoading] = useState<boolean>(true)
 
-    }, [teamId])
+  useEffect(() => {
+    if (!teamId) return
+    console.log(teamId)
+    getTeamDetail(teamId)
+      .then((response) => {
+        if ((response as ErrorResponse).error) {
+          console.log(response)
+          setError(response as ErrorResponse)
+        } else {
+          console.log(response)
+          setTeamDetailData(response as TeamDetailData)
+        }
+      })
+      .finally(() => setLoading(false))
+      .catch((error) => {
+        setError(error.toString())
+      })
+  }, [teamId])
 
-
-    return <PageLayout title='Teamoversikt' content={<h1>TeamDetail</h1>} />
+  return <PageLayout title='Teamoversikt' content={<h1>TeamDetail</h1>} />
 }
