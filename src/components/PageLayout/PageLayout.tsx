42d7a43d03ab59618cde8a65838328c1311a097a
ssb-jnk <jnk@ssb.no>
2024-02-09 08:52:23 +0100

    some more stuff to review

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -3,7 +3,7 @@ import Header from '../Header/Header'
 import SidebarModal from '../SidebarModal/SidebarModal'
 import styles from './pagelayout.module.scss'
 
-import { Title, LeadParagraph, Input, Dialog, Dropdown } from '@statisticsnorway/ssb-component-library'
+import { Title, LeadParagraph } from '@statisticsnorway/ssb-component-library'
 
 interface PageLayoutProps {
   title: string
@@ -21,34 +21,7 @@ export default function PageLayout({ title, description, button, content }: Page
         {/* TODO: Remove after testing; or implement a temporary button that toggles this in one of the pages */}
         <SidebarModal
         header={{modalType: "Medlem", modalTitle: "Arbmark register", modalDescription: "arbmark-register"}}
-        footer={{submitButtonText: "Legg til medlem"}}
-        body={
-          <div>
-            <h2>Legg person til teamet</h2>
-            <p>Navn</p>
-            <Input placeholder="Skriv navn..." />
-
-            <p>Rolle(r)</p>
-            <Dropdown 
-              placeholder="Velg rolle" 
-              searchable
-              selectedItem={{title: "Ingen tilgang", id: "ingen-tilgang"}}
-              items={
-              [
-                {title: "Managers", id: "managers"},
-                {title: "Developers", id: "developers"},
-                {title: "Data-admins", id: "data-admins"},
-                {title: "Ingen tilgang", id: "ingen-tilgang"},
-                
-              ]
-            }/>
-
-            <div className={styles.modalInfoDialog}></div>
-            <Dialog type='info' title="Tidskrevende jobb!">
-              Det kan ta litt tid før du ser endringen.
-            </Dialog>
-          </div>
-        }
+        footer={{submitButtonText: "Legg til medlem", handleSubmit: () => {console.log("clicked")}}}
         />
         <div className={styles.title}>
           <Title size={1}>{title}</Title>
--- src/components/SidebarModal/SidebarModal.tsx
+++ src/components/SidebarModal/SidebarModal.tsx
@@ -1,7 +1,7 @@
 import styles from './sidebar.module.scss'
 
 import { useState } from 'react'
-import { Title, Link, Button } from '@statisticsnorway/ssb-component-library'
+import { Link, Button, Dialog, Input, Dropdown } from '@statisticsnorway/ssb-component-library'
 import { X } from 'react-feather'
 
 interface SidebarHeader {
@@ -23,13 +23,18 @@ const SidebarModalHeader = ({modalType, modalTitle, modalDescription}: SidebarHe
 interface SidebarFooter {
   submitButtonText: string
   handleClose?: () => void
+  handleSubmit?: () => void
 }
 
-const SidebarModalFooter = ({submitButtonText, handleClose}: SidebarFooter): JSX.Element => {
+const SidebarModalFooter = ({submitButtonText, handleClose, handleSubmit}: SidebarFooter): JSX.Element => {
   return (
     <div className={styles.modalFooter}>
       <Link onClick={handleClose}>Avbryt</Link>
-      <div className={styles.modalFooterButtonText}><Button primary>{submitButtonText}</Button></div>
+      <div className={styles.modalFooterButtonText}>
+        <Button onClick={handleSubmit} primary>
+          {submitButtonText}
+        </Button>
+      </div>
     </div>
   )
 }
@@ -59,6 +64,31 @@ const SidebarModal = ({ header, footer, body }: SidebarModal) => {
         <SidebarModalHeader {...header} />
         <div className={styles.body}>
           {/* Form goes here */}
+          <div>
+            <h2>Legg person til teamet</h2>
+            <p>Navn</p>
+            <Input placeholder="Skriv navn..." />
+
+            <p>Rolle(r)</p>
+            <Dropdown 
+              placeholder="Velg rolle" 
+              searchable
+              selectedItem={{title: "Ingen tilgang", id: "ingen-tilgang"}}
+              items={
+              [
+                {title: "Managers", id: "managers"},
+                {title: "Developers", id: "developers"},
+                {title: "Data-admins", id: "data-admins"},
+                {title: "Ingen tilgang", id: "ingen-tilgang"},
+                
+              ]
+            }/>
+
+            <div className={styles.modalInfoDialog}></div>
+            <Dialog type='info' title="Tidskrevende jobb!">
+              Det kan ta litt tid før du ser endringen.
+            </Dialog>
+          </div>
           {body}
         </div>
         <SidebarModalFooter {...footer} handleClose={footer.handleClose ? footer.handleClose : handleClose} />
