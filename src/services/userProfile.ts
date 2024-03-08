c04a431eb7595b97a5652e9079254ec6ffcb28b5
ssb-jnk <jnk@ssb.no>
2024-03-08 09:53:41 +0100

    add-usersUrl

--- src/services/userProfile.ts
+++ src/services/userProfile.ts
@@ -49,7 +49,7 @@ interface Group {
 
 export const getUserProfile = async (principalName: string): Promise<User | ApiError> => {
   const usersUrl = new URL(`${USERS_URL}/${principalName}`)
-
+  console.log(usersUrl)
   const embeds = ['section_manager']
   const selects = [
     'principal_name',
