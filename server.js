9a59d3e2b86a0bd3def4c2a1a52e0909f9a6d086
ssb-jnk <jnk@ssb.no>
2024-03-09 21:28:30 +0100

    lint

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ if (!process.env.DAPLA_TEAM_API_URL) {
 
 const app = express()
 const PORT = process.env.PORT || 3000
-const DAPLA_TEAM_API_URL = process.env.DAPLA_TEAM_API_URL || "https://dapla-team-api-v2.staging-bip-app.ssb.no"
+const DAPLA_TEAM_API_URL = process.env.DAPLA_TEAM_API_URL || 'https://dapla-team-api-v2.staging-bip-app.ssb.no'
 
 // Proxy, note this middleware must be place before all else.. THIS TOOK ME 3 HOURS TO FIGURE OUT! TODO: Remove comment
 app.use(
