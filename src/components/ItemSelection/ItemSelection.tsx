083da070c05d9827170c133776817645897a3057
Nicholas Jaunsen <3789764+skykanin@users.noreply.github.com>
2024-06-14 10:25:39 +0200

    Implement ItemSelection component
    This is useful for selecting multiple items from a list.
    The component isn't in use right now, but it will be later
    when we add support for selecting 'features' from the create
    team page.

---
+++ src/components/ItemSelection/ItemSelection.tsx
@@ -0,0 +1,50 @@
+import { useMemo, useState } from 'react'
+import { Tag } from '@statisticsnorway/ssb-component-library'
+import styles from './itemSelection.module.scss'
+
+export interface Item {
+    id: number
+    text: string
+}
+
+interface ItemSelectionProps {
+    header: string
+    items: Item[]
+    onSelectionChange: (selectedItems: Item[]) => void
+}
+function ItemSelection({ header, items, onSelectionChange }: ItemSelectionProps) {
+    const [selectedItems, setSelectedItems] = useState<Item[]>([])
+
+    const selectedItemIds = useMemo(() => selectedItems.map((item) => item.id), [selectedItems])
+
+    const selectItem = (selectedItem: Item) => {
+        const newSelectedItems = selectedItemIds.includes(selectedItem.id)
+            ? selectedItems.filter((item) => item.id !== selectedItem.id)
+            : [...selectedItems, selectedItem]
+
+        setSelectedItems(newSelectedItems)
+        onSelectionChange(newSelectedItems)
+    }
+
+    return (
+        <div className={styles.rootComponent}>
+            <div className={styles.header}>{header}</div>
+            <div className={styles.itemSelection}>
+                {items.map(({ id, text }) => (
+                    <Tag
+                        key={id}
+                        onClick={(event: Event) => {
+                            selectItem({ id, text })
+                            event.preventDefault()
+                        }}
+                        className={selectedItemIds.includes(id) ? styles.selectedItem : ''}
+                    >
+                        {text}
+                    </Tag>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default ItemSelection
