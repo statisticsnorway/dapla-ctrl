d5a54662e6676299d7ae57b40ae1efdeaf7db462
skykanin <3789764+skykanin@users.noreply.github.com>
2024-08-07 09:30:21 +0200

    add custom logger
    the custom logger logs messages to the proxy server
    which allows us to see logged messages in google cloud
    logs as `console.log` calls in the frontend won't appear there

---
+++ src/utils/logger.ts
@@ -0,0 +1,13 @@
+import { Console, Effect, Logger } from 'effect'
+import * as Http from '@effect/platform/HttpClient'
+
+const logger = Logger.make(({ logLevel, message }) => {
+  const logMsg = `[${logLevel.label}] ${message}`
+  globalThis.console.log(logMsg)
+  Console.log(logMsg).pipe(
+    Effect.zipRight(Http.request.post('/log').pipe(Http.request.textBody(logMsg), Http.client.fetchOk, Effect.scoped)),
+    Effect.runPromise
+  )
+})
+
+export const customLogger = Logger.replace(Logger.defaultLogger, logger)
