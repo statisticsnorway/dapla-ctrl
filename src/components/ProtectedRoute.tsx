e7ed0d341e4645ab360207cbdb3990ea2218e555
Nicholas <3789764+skykanin@users.noreply.github.com>
2024-12-11 10:06:32 +0100

    fix bug in protected routes components (#377)
    * fix bug in protected routes components
    
    routing is now relative and zustand is used
    to share state between components
    
    * add new store file...
    
    * fix type error
    
    * fixup formatting
    
    * fixup useEffect hook
    
    * fix linting errors
    
    * change setter name to be more domain specific
    
    * fixup

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -5,27 +5,28 @@ import { fetchUserInformationFromAuthToken } from '../utils/services'
 import { Cause, Effect, Option as O } from 'effect'
 import { customLogger } from '../utils/logger.ts'
 import { ApiError } from '../utils/services.ts'
+import { useUserProfileStore } from '../services/store.ts'
 
 import { User } from '../services/userProfile.ts'
 
 const ProtectedRoute = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const setLoggedInUser = useUserProfileStore((state) => state.setLoggedInUser)
   const navigate = useNavigate()
   const from = location.pathname
 
-  const fetchUserProfile = (): Effect.Effect<void, Cause.UnknownException | ApiError> =>
-    Effect.gen(function* () {
-      const userProfileData = yield* Effect.promise(fetchUserInformationFromAuthToken)
-      const userProfile = yield* Effect.tryPromise(() => getUserProfile(userProfileData.email)).pipe(
-        Effect.flatMap((x) => (x instanceof ApiError ? Effect.fail(x) : Effect.succeed(x))),
-        Effect.map(JSON.stringify)
-      )
-      yield* Effect.logInfo(`UserProfile set in localStorage: ${userProfile}`)
-      yield* Effect.sync(() => localStorage.setItem('userProfile', userProfile))
-      yield* Effect.sync(() => setIsAuthenticated(true))
-    }).pipe(Effect.provide(customLogger))
-
   useEffect(() => {
+    const fetchUserProfile = (): Effect.Effect<void, Cause.UnknownException | ApiError> =>
+      Effect.gen(function* () {
+        const userProfileData = yield* Effect.promise(fetchUserInformationFromAuthToken)
+        const userProfile = yield* Effect.tryPromise(() => getUserProfile(userProfileData.email)).pipe(
+          Effect.flatMap((x) => (x instanceof ApiError ? Effect.fail(x) : Effect.succeed(x)))
+        )
+        yield* Effect.sync(() => localStorage.setItem('userProfile', JSON.stringify(userProfile)))
+        yield* Effect.sync(() => setLoggedInUser(userProfile))
+        yield* Effect.sync(() => setIsAuthenticated(true))
+      }).pipe(Effect.provide(customLogger))
+
     const cachedUserProfile: O.Option<User> = O.fromNullable(localStorage.getItem('userProfile')).pipe(
       O.flatMap(O.liftThrowable(JSON.parse))
     )
@@ -34,13 +35,16 @@ const ProtectedRoute = () => {
       onSome: (userProfile) =>
         // invalidate cached user profile if 'job_title' field is missing
         userProfile.job_title
-          ? Effect.sync(() => setIsAuthenticated(true))
+          ? Effect.zip(
+              Effect.sync(() => setIsAuthenticated(true)),
+              Effect.sync(() => setLoggedInUser(userProfile))
+            )
           : Effect.zipRight(
               Effect.logInfo("'job_title' field missing, invalidating UserProfile cache"),
               fetchUserProfile()
             ).pipe(Effect.provide(customLogger)),
     }).pipe(Effect.runPromise)
-  }, [from, navigate])
+  }, [from, navigate, setLoggedInUser])
 
   return isAuthenticated ? <Outlet /> : null
 }
