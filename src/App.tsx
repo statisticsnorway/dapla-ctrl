58d978dd3af4341bf75cbffdef4c4269d9b974d1
John Kasper Svergja <johnksv@users.noreply.github.com>
2025-05-30 09:51:19 +0200

    Remove Teammedlemmer as page and from route (#437)
    Since this page crashes the team api due to the heavy load of the query
    and no pagination
    
    On request by PO

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import NotFound from './pages/NotFound/NotFound.tsx'
 import TeamOverview from './pages/TeamOverview/TeamOverview'
 import UserProfile from './pages/UserProfile/UserProfile'
 import TeamDetail from './pages/TeamDetail/TeamDetail'
-import TeamMembers from './pages/TeamMembers/TeamMembers'
 import SharedBucketDetail from './pages/SharedBucketDetail/SharedBucketDetail.tsx'
 
 import { Routes, Route } from 'react-router-dom'
@@ -17,7 +16,7 @@ const App = () => {
     <Routes>
       <Route element={<ProtectedRoute />}>
         <Route path='/' element={<TeamOverview />} />
-        <Route path='/teammedlemmer' element={<TeamMembers />} />
+        {/*<Route path='/teammedlemmer' element={<TeamMembers />} />*/}
         <Route path='/teammedlemmer/:principalName' element={<UserProfile />} />
         <Route path='/:teamId' element={<TeamDetail />} />
         <Route path='/:teamId/:shortName' element={<SharedBucketDetail />} />
