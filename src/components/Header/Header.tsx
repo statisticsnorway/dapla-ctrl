e509d0057ada512d3ac6f35139b61378c44d7e02
John Kasper Svergja <johnkasper.svergja@ssb.no>
2025-05-30 09:37:07 +0200

    Remove Teammedlemmer as page and from route
    Since this page crashes the team api due to the heavy load of the query
    and no pagination
    
    On request by PO

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import { Link } from '@statisticsnorway/ssb-component-library'
 import Avatar from '../Avatar/Avatar'
 import { useNavigate } from 'react-router-dom'
 
-import { Book, Users } from 'react-feather'
+import { Book } from 'react-feather'
 
 const Header = () => {
   const navigate = useNavigate()
@@ -18,9 +18,6 @@ const Header = () => {
         <div className={styles.links}>
           <Link href={import.meta.env.DAPLA_CTRL_DOCUMENTATION_URL ?? ''} isExternal={true} icon={<Book size='20' />}>
             Dokumentasjon
-          </Link>
-          <Link href='/teammedlemmer' icon={<Users size='20' />}>
-            Teammedlemmer
           </Link>
         </div>
         <Avatar />
