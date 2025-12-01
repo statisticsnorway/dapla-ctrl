157fa998c893a453ba37d4f9c633ca7637cd1d5a
skykanin <3789764+skykanin@users.noreply.github.com>
2025-12-01 09:37:45 +0100

    update nixpkgs ref

--- flake.nix
+++ flake.nix
@@ -11,7 +11,6 @@
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
       perSystem = {
         pkgs,
-        self',
         ...
       }: {
         devShells.default = pkgs.mkShell {
