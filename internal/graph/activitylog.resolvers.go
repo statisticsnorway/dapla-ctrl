73f3e32a2da0cdb4a18948410c09167b1bc93c4a
nicolst <35232838+nicolst@users.noreply.github.com>
2025-12-16 14:03:58 +0100

    chore: fmt

--- internal/graph/activitylog.resolvers.go
+++ internal/graph/activitylog.resolvers.go
@@ -9,6 +9,15 @@ import (
 	"github.com/statisticsnorway/dapla-api/internal/team"
 )
 
+func (r *queryResolver) ActivityLog(ctx context.Context, first *int, after *pagination.Cursor, last *int, before *pagination.Cursor) (*pagination.Connection[activitylog.ActivityLogEntry], error) {
+	page, err := pagination.ParsePage(first, after, last, before)
+	if err != nil {
+		return nil, err
+	}
+
+	return activitylog.List(ctx, page)
+}
+
 func (r *reconcilerResolver) ActivityLog(ctx context.Context, obj *reconciler.Reconciler, first *int, after *pagination.Cursor, last *int, before *pagination.Cursor) (*pagination.Connection[activitylog.ActivityLogEntry], error) {
 	page, err := pagination.ParsePage(first, after, last, before)
 	if err != nil {
@@ -25,13 +34,4 @@ func (r *teamResolver) ActivityLog(ctx context.Context, obj *team.Team, first *i
 	}
 
 	return activitylog.ListForTeam(ctx, obj.Slug, page)
-}
-
-func (r *queryResolver) ActivityLog(ctx context.Context, first *int, after *pagination.Cursor, last *int, before *pagination.Cursor) (*pagination.Connection[activitylog.ActivityLogEntry], error) {
-	page, err := pagination.ParsePage(first, after, last, before)
-	if err != nil {
-		return nil, err
-	}
-
-	return activitylog.List(ctx, page)
 }
