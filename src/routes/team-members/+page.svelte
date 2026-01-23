f305ec4ad5558d8a62e1050a278a87eba32052d2
Nicolai Stølen <35232838+nicolst@users.noreply.github.com>
2026-01-23 17:24:49 +0100

    fix: load users correctly (#61)

--- src/routes/team-members/+page.svelte
+++ src/routes/team-members/+page.svelte
@@ -1,46 +1,50 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
 	import TeamMembersTable from './TeamMembersTable.svelte';
-	import { type TeamMemberData } from './teamMembersUtils';
 	import { changeParams } from '$lib/utils/searchparams';
+	import type { MyTeamMembers$result } from '$houdini';
+	import type { TeamMemberData } from './teamMembersUtils';
 
 	let { data }: PageProps = $props();
 	let { MyTeamMembers } = $derived(data);
+
+	type TeamMember = Extract<
+		MyTeamMembers$result['me'],
+		{ __typename: 'User' }
+	>['teamMembers']['nodes'][0];
 
 	const changeQuery = (params: { after?: string; before?: string }) => {
 		changeParams(params);
 	};
 
-	let meUser = $MyTeamMembers.data?.me.__typename === 'User' ? $MyTeamMembers.data?.me : undefined;
-
-	let teamMembers: TeamMemberData[] =
-		meUser?.teamMembers.nodes.map((u) => {
-			return {
-				user: {
-					name: u.name,
-					email: u.email
-				},
-				teamCount: u.teams.pageInfo.totalCount,
-				dataAdminCount: u.dataAdmins.pageInfo.totalCount,
-				sectionManager: u.section?.manager
-					? {
-							name: u.section.manager.name,
-							email: u.section.manager.email
-						}
-					: undefined
-			};
-		}) ?? [];
+	function transformTeamMembersData(u: TeamMember): TeamMemberData {
+		return {
+			user: {
+				name: u.name,
+				email: u.email
+			},
+			teamCount: u.teams.pageInfo.totalCount,
+			dataAdminCount: u.dataAdmins.pageInfo.totalCount,
+			sectionManager: u.section?.manager
+				? {
+						name: u.section.manager.name,
+						email: u.section.manager.email
+					}
+				: undefined
+		};
+	}
 </script>
 
 {#if $MyTeamMembers.fetching}
 	<p>Laster...</p>
 {:else if $MyTeamMembers.data?.me.__typename === 'User'}
 	<TeamMembersTable
-		{teamMembers}
+		teamMembers={$MyTeamMembers.data.me.teamMembers.nodes.map(transformTeamMembersData)}
 		pageInfo={$MyTeamMembers.data.me.teamMembers.pageInfo}
 		loaders={{
 			loadPreviousPage: () => {
 				changeQuery({
+					after: '',
 					before:
 						$MyTeamMembers.data?.me.__typename === 'User'
 							? ($MyTeamMembers.data?.me.teamMembers.pageInfo.startCursor ?? '')
@@ -49,6 +53,7 @@
 			},
 			loadNextPage: () => {
 				changeQuery({
+					before: '',
 					after:
 						$MyTeamMembers.data?.me.__typename === 'User'
 							? ($MyTeamMembers.data?.me.teamMembers.pageInfo.endCursor ?? '')
--- src/routes/team-members/all/+page.svelte
+++ src/routes/team-members/all/+page.svelte
@@ -2,48 +2,46 @@
 	import { changeParams } from '$lib/utils/searchparams';
 	import type { PageProps } from './$types';
 	import TeamMembersTable from '../TeamMembersTable.svelte';
+	import type { TeamMemberData } from '../teamMembersUtils';
+	import type { AllTeamMembers$result } from '$houdini';
 
 	let { data }: PageProps = $props();
 	let { AllTeamMembers } = $derived(data);
+
+	type TeamMember = AllTeamMembers$result['users']['nodes'][0];
 
 	const changeQuery = (params: { after?: string; before?: string }) => {
 		changeParams(params);
 	};
 
-	let teamMembers = $derived.by(() => {
-		if (!$AllTeamMembers.data) {
-			return [];
-		}
-
-		return $AllTeamMembers.data.users.nodes.map((u) => {
-			return {
-				user: {
-					name: u.name,
-					email: u.email
-				},
-				teamCount: u.teams.pageInfo.totalCount,
-				dataAdminCount: u.dataAdmins.pageInfo.totalCount,
-				sectionManager: u.section?.manager
-					? {
-							name: u.section.manager.name,
-							email: u.section.manager.email
-						}
-					: undefined
-			};
-		});
-	});
+	function transformTeamMembersData(u: TeamMember): TeamMemberData {
+		return {
+			user: {
+				name: u.name,
+				email: u.email
+			},
+			teamCount: u.teams.pageInfo.totalCount,
+			dataAdminCount: u.dataAdmins.pageInfo.totalCount,
+			sectionManager: u.section?.manager
+				? {
+						name: u.section.manager.name,
+						email: u.section.manager.email
+					}
+				: undefined
+		};
+	}
 </script>
 
 {#if $AllTeamMembers.data}
 	<TeamMembersTable
-		{teamMembers}
+		teamMembers={$AllTeamMembers.data?.users.nodes.map(transformTeamMembersData)}
 		pageInfo={$AllTeamMembers.data.users.pageInfo}
 		loaders={{
 			loadPreviousPage: () => {
-				changeQuery({ before: $AllTeamMembers.data?.users.pageInfo.startCursor ?? '' });
+				changeQuery({ after: '', before: $AllTeamMembers.data?.users.pageInfo.startCursor ?? '' });
 			},
 			loadNextPage: () => {
-				changeQuery({ after: $AllTeamMembers.data?.users.pageInfo.endCursor ?? '' });
+				changeQuery({ before: '', after: $AllTeamMembers.data?.users.pageInfo.endCursor ?? '' });
 			}
 		}}
 	/>
