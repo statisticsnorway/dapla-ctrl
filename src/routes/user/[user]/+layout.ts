2387e454410235d6bbea84027e3b4ab870b7106a
Nicolai Stølen <35232838+nicolst@users.noreply.github.com>
2026-01-28 15:07:52 +0100

    feat: show user display name in breadcrumb (#75)

--- src/routes/user/[user]/+layout.ts
+++ src/routes/user/[user]/+layout.ts
@@ -1,13 +1,23 @@
+import { load_UserLayoutInfo } from '$houdini';
 import { addPageMeta } from '$lib/utils/pageMeta.js';
+import { get } from 'svelte/store';
 
 export async function load(event) {
+	const { UserLayoutInfo } = await load_UserLayoutInfo({
+		event,
+		blocking: true,
+		variables: { user: event.params.user }
+	});
+
+	const name = get(UserLayoutInfo).data?.user.name ?? event.params.user;
+
 	const meta = await addPageMeta(event, {
 		breadcrumbs:
 			event.route.id === '/user/[user]'
 				? undefined
 				: [
 						{
-							label: event.params.user,
+							label: name,
 							href: '/user/[user]'
 						}
 					]
