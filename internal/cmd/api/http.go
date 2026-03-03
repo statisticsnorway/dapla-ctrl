b34b0016a24cec23453c04bdc29e5d5c63f51684
Nicolai Stølen <35232838+nicolst@users.noreply.github.com>
2026-03-03 13:55:36 +0100

    fix: use correct version of otel/semconv (#78)

--- internal/cmd/api/http.go
+++ internal/cmd/api/http.go
@@ -32,7 +32,7 @@ import (
 	"github.com/statisticsnorway/dapla-api/internal/user"
 	"github.com/statisticsnorway/dapla-api/internal/usersync"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
-	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.39.0"
 	"go.opentelemetry.io/otel/trace"
 	"golang.org/x/sync/errgroup"
 )
