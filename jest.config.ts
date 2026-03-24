569bae835df3bcf570936fba819a5122680d721d
skykanin <3789764+skykanin@users.noreply.github.com>
2026-03-24 14:39:06 +0100

    fix jest config for ESM and TypeScript 6

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,22 @@
-module.exports = {
-  preset: 'ts-jest',
+import type { Config } from 'jest'
+
+const config: Config = {
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          moduleResolution: 'bundler',
+          esModuleInterop: true,
+          allowSyntheticDefaultImports: true,
+          ignoreDeprecations: '6.0',
+        },
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
 }
+
+export default config
