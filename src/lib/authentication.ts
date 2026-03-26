2c8c19bd2ce25a75ffbba473eadc05633d9c5355
Weihai Pan <36296800+ssbwep@users.noreply.github.com>
2026-03-26 11:03:03 +0100

    fix: force a redirect to make user logged in (#131)
    Internal ref: [DPSKY-1352]

--- src/lib/authentication.ts
+++ src/lib/authentication.ts
@@ -1,5 +1,7 @@
 import type { ClientPlugin } from '$houdini';
 import { writable } from 'svelte/store';
+import { redirect } from '@sveltejs/kit';
+import { page } from '$app/state';
 
 export const isAuthenticated = writable<boolean>(true);
 
@@ -21,6 +23,11 @@ export const handleMissingLogin = (...ignoredNames: string[]): ClientPlugin => {
 			afterNetwork(ctx, { value, resolve }) {
 				if (!ignoredNames.includes(ctx.name) && isUnauthenticated(value.errors)) {
 					isAuthenticated.set(false);
+					const redirectPath = (url: URL) => {
+						return encodeURIComponent(url.pathname + url.search + url.hash);
+					};
+					const oauth2LoginPath = '/oauth2/login?redirect_uri=' + redirectPath(page.url);
+					redirect(302, oauth2LoginPath);
 				} else if (ctx.name == 'UserInfo' && value.data) {
 					if (value.data.me) {
 						isAuthenticated.set(true);
