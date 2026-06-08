abf8a7d5676d8a85d0b1aa8083f043d26553ac81
John Kasper Svergja <johnkasper.svergja@ssb.no>
2026-06-08 09:42:56 +0200

    feat(frontend): Add gui for manuel editing
    [DPSKY-1421]

--- frontend/src/routes/team/[team]/settings/+page.svelte
+++ frontend/src/routes/team/[team]/settings/+page.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
 	import { graphql } from '$houdini';
 	import GraphErrors from '$lib/ui/GraphErrors.svelte';
-	import { Heading } from '@nais/ds-svelte-community';
+	import { Heading, Switch } from '@nais/ds-svelte-community';
 	import type { PageProps } from './$types';
-	import EditText from './EditText.svelte';
+	import Confirm from '$lib/ui/Confirm.svelte';
 
 	let { data }: PageProps = $props();
-	let { TeamSettings, teamSlug, viewerIsMember } = $derived(data);
+	let { TeamSettings, teamSlug } = $derived(data);
+	let showConfirmModal = $state(false);
 
 	const updateTeam = graphql(`
 		mutation UpdateTeam($input: UpdateTeamInput!) {
 			updateTeam(input: $input) {
 				team {
-					displayName
+					hasManualEditing
 				}
 			}
 		}
@@ -21,6 +22,20 @@
 	let teamSettings = $derived($TeamSettings.data?.team);
 
 	let descriptionErrors: { message: string }[] | undefined = $state();
+
+	const toggle = async () => {
+		descriptionErrors = undefined;
+		const data = await updateTeam.mutate({
+			input: {
+				slug: teamSlug,
+				hasManualEditing: !$TeamSettings.data?.team.hasManualEditing
+			}
+		});
+
+		if (data.errors) {
+			descriptionErrors = data.errors;
+		}
+	};
 </script>
 
 <GraphErrors errors={$TeamSettings.errors} />
@@ -29,30 +44,39 @@
 	<div class="wrapper">
 		<div style="display: flex; flex-direction: column; gap: var(--spacing-layout)">
 			<div>
-				<Heading level="2">Visningsnavn</Heading>
-				<EditText
-					text={teamSettings.displayName}
-					onsave={async (text) => {
-						descriptionErrors = undefined;
-						const data = await updateTeam.mutate({
-							input: {
-								slug: teamSlug,
-								displayName: text
-							}
-						});
+				<Heading level="2">Parquedit</Heading>
 
-						if (data.errors) {
-							descriptionErrors = data.errors;
-						}
+				Parquedit er en lagringsløsning for manuell editering, levert av team Dapla
+				Fellesfunksjoner.
+
+				<Switch
+					checked={teamSettings.hasManualEditing}
+					onclick={(e: MouseEvent) => {
+						e.preventDefault();
+						showConfirmModal = true;
 					}}
-					isMember={viewerIsMember}
-				/>
+					>{teamSettings.hasManualEditing
+						? 'Fjern tilgang til Parquedit'
+						: 'Aktiver tilgang til Parquedit'}</Switch
+				>
 
 				<GraphErrors errors={descriptionErrors} size="small" />
 			</div>
 		</div>
 	</div>
 {/if}
+
+<Confirm bind:open={showConfirmModal} onconfirm={toggle} confirmText="Bekreft">
+	{#snippet header()}
+		<h3>Parquedit bekreftelse</h3>
+	{/snippet}
+
+	Dette vil <b>{$TeamSettings.data?.team.hasManualEditing ? 'slå av' : 'slå på'}</b> Parquedit.<br
+	/>
+	{$TeamSettings.data?.team.hasManualEditing
+		? 'Historikken knyttet til manuell editering vil da bli slettet.'
+		: ''}
+</Confirm>
 
 <style>
 	.wrapper {
