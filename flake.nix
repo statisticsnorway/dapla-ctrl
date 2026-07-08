0868dcf55f211ebaf5ae50cf5618de9a19b5f626
skykanin <3789764+skykanin@users.noreply.github.com>
2026-07-08 15:01:26 +0200

    nix: add flake.nix
    includes a development environment
    for the dapla-api and dapla-ctrl frontend

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Development environment for the dapla API and frontend";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { pkgs, ... }: {
+
+        devShells.default = pkgs.mkShell {
+          name = "dapla devenv";
+
+          packages = with pkgs; [
+            pnpm
+            protobuf-language-server
+            nixd
+            nodejs
+            mise
+            go
+            gopls
+            yaml-language-server
+          ];
+        };
+
+        formatter = pkgs.alejandra;
+      };
+    };
+}
