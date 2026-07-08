27719dee31aa9d648c976197a6f123cbb0041020
skykanin <3789764+skykanin@users.noreply.github.com>
2026-07-08 15:32:00 +0200

    add workflow as well

--- flake.nix
+++ flake.nix
@@ -6,15 +6,15 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { pkgs, ... }: {
-
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {pkgs, ...}: {
         devShells.default = pkgs.mkShell {
           name = "dapla devenv";
 
           packages = with pkgs; [
+            actionlint
             go
             gopls
             mise
