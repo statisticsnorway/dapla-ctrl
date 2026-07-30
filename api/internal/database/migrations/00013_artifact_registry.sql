a53ec149960d786e852573a91341c2c14be9effc
nicolst <35232838+nicolst@users.noreply.github.com>
2026-07-30 10:14:05 +0200

    chore: fmt

--- api/internal/database/migrations/00013_artifact_registry.sql
+++ api/internal/database/migrations/00013_artifact_registry.sql
@@ -1,14 +1,14 @@
 -- +goose Up
 CREATE TABLE team_repositories (
-    team_slug slug NOT NULL,
-    github_repository TEXT NOT NULL,
-    PRIMARY KEY (team_slug, github_repo)
+	team_slug slug NOT NULL,
+	github_repository TEXT NOT NULL,
+	PRIMARY KEY (team_slug, github_repo)
 )
 ;
 
 CREATE TABLE team_artifact_registry_repositories (
-    team_slug slug NOT NULL REFERENCES teams (slug) ON DELETE CASCADE,
-    format string NOT NULL,
-    size_bytes BIGINT NOT NULL
+	team_slug slug NOT NULL REFERENCES teams (slug) ON DELETE CASCADE,
+	format string NOT NULL,
+	size_bytes BIGINT NOT NULL
 )
 ;
