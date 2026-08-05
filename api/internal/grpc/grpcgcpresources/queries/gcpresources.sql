7212e089f9bd8022ff11dac0d6ad01a230d1bb80
skykanin <3789764+skykanin@users.noreply.github.com>
2026-08-05 14:28:06 +0200

    smaller changes

--- api/internal/grpc/grpcgcpresources/queries/gcpresources.sql
+++ api/internal/grpc/grpcgcpresources/queries/gcpresources.sql
@@ -3,12 +3,16 @@ INSERT INTO
 	gcp_team_folders (team_slug, env, folder_id)
 VALUES
 	($1, $2, $3)
-ON CONFLICT (team_slug, env) DO UPDATE SET folder_id = EXCLUDED.folder_id
+ON CONFLICT (team_slug, env) DO UPDATE
+SET
+	folder_id = EXCLUDED.folder_id
 ;
 
 -- name: GetTeamFolder :one
 SELECT
-	team_slug, env, folder_id
+	team_slug,
+	env,
+	folder_id
 FROM
 	gcp_team_folders
 WHERE
