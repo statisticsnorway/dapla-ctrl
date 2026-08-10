5ba21e05eccc7ce6f994de5928b149c8ad7e6607
Weihai Pan <36296800+ssbwep@users.noreply.github.com>
2026-08-10 14:19:45 +0200

    chore(api/reconciler): refactor

--- api/internal/grpc/grpcgcpresources/server.go
+++ api/internal/grpc/grpcgcpresources/server.go
@@ -53,10 +53,3 @@ func (s *Server) GetTeamFolder(ctx context.Context, req *protoapi.GetGcpTeamFold
 		},
 	}, nil
 }
-
-func (s *Server) DeleteTeamFolders(ctx context.Context, req *protoapi.DeleteGcpTeamFoldersRequest) (*protoapi.DeleteGcpTeamFoldersResponse, error) {
-	if err := s.querier.DeleteTeamFolders(ctx, slug.Slug(req.TeamSlug)); err != nil {
-		return nil, status.Errorf(codes.Internal, "delete team folders: %s", err)
-	}
-	return &protoapi.DeleteGcpTeamFoldersResponse{}, nil
-}
