969a59d3b8a0dc91407bc0837413d264c3ff4118
JibJuke <45637114+JibJuke@users.noreply.github.com>
2026-08-12 12:29:23 +0200

    fix(frontend)improve Parquedit activitylog text

--- frontend/src/lib/domain/activity/shared/texts/TeamUpdatedActivityLogEntryText.svelte
+++ frontend/src/lib/domain/activity/shared/texts/TeamUpdatedActivityLogEntryText.svelte
@@ -21,23 +21,24 @@
 				return fieldName;
 		}
 	};
-	const fieldValueToDisplayValue = (fieldName: string, value: string | null | undefined) => {
+	const updatedFieldToDisplayText = (fieldName: string, value: string | null | undefined) => {
 		if (fieldName === 'hasManualEditing') {
-			if (value === 'true') return 'aktivert';
-			if (value === 'false') return 'deaktivert';
+		    return value === 'true'
+				? 'Parquedit ble skrudd på'
+				: 'Parquedit ble skrudd av';
 		}
 		return value;
 	};
 </script>
 
 <div>
-	{#if data.teamUpdated?.updatedFields.length}
-		{#each data.teamUpdated?.updatedFields as field (field)}
-			{fieldNameToDisplayName(field.field)} endret fra
-			<i>{fieldValueToDisplayValue(field.field, field.oldValue)}</i> til
-			<i>{fieldValueToDisplayValue(field.field, field.newValue)}</i>
-		{/each}
-	{/if}
+	{#each data.teamUpdated?.updatedFields as field(field)}
+		{#if updatedFieldToDisplayText(field.field, field.newValue)}
+		    {updatedFieldToDisplayText(field.field, field.newValue)}
+		{:else}
+			{fieldNameToDisplayName(field.field)} endret fra <i>(field.oldValue</i> til<i>field.newValue</i>
+		{/if}
+	{/each}
 
 	<BodyShort textColor="subtle" size="small">
 		av {data.actor} for
