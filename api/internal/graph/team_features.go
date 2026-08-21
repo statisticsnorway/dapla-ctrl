85c1616d8d250704cc88b4bbde0ba52dcca4d82d
skykanin <3789764+skykanin@users.noreply.github.com>
2026-08-21 14:01:50 +0200

    fix(api): use enums in team feature payloads

---
+++ api/internal/graph/team_features.go
@@ -0,0 +1,27 @@
+package graph
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+)
+
+func validateTeamFeatureArgs(feature string, env string) error {
+	validFeatures := []string{"ai"}
+	validEnvs := []string{"prod", "test"}
+
+	formatError := func(element string, value string, expectedValues []string) error {
+		return fmt.Errorf("validateTeamFeatureArgs: Invalid value for %s %q, must be one of %q", element, value, strings.Join(expectedValues, ","))
+	}
+
+	if !slices.Contains(validFeatures, feature) {
+		return formatError("feature", feature, validFeatures)
+	}
+	if !slices.Contains(validEnvs, env) {
+		return formatError("env", env, validEnvs)
+	}
+	if feature == "ai" && env != "test" {
+		return fmt.Errorf("validateTeamFeatureArgs: Invalid combinations of values feature: %q and env: %q", feature, env)
+	}
+	return nil
+}
