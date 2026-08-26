266c5bf9b625848a6b29b45e2177ad37f2d7bedc
nicolst <35232838+nicolst@users.noreply.github.com>
2026-08-26 10:34:03 +0200

    feat(frontend): custom activity log text for feature toggles

---
+++ frontend/src/lib/domain/activity/shared/texts/TeamFeatureToggledActivityLogEntryText.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Time from '$lib/ui/Time.svelte';
+	import { capitalizeFirstLetter } from '$lib/utils/formatters';
+	import type { ActivityLogEntry } from './types';
+	import { BodyShort } from '@nais/ds-svelte-community';
+
+	let {
+		data
+	}: {
+		data: ActivityLogEntry<
+			'TeamFeatureEnabledActivityLogEntry' | 'TeamFeatureDisabledActivityLogEntry'
+		>;
+	} = $props();
+
+	const featureToDisplayName = (feature: string) => {
+		switch (feature) {
+			case 'ai':
+				return 'AI-funksjonalitet';
+			default:
+				return capitalizeFirstLetter(feature);
+		}
+	};
+</script>
+
+<div>
+	{featureToDisplayName(data.resourceName)} ble slått
+	{data.__typename === 'TeamFeatureEnabledActivityLogEntry' ? 'på' : 'av'}
+	i {data.env}
+
+	<BodyShort textColor="subtle" size="small">
+		av {data.actor} for
+		<Time time={data.createdAt} distance />
+	</BodyShort>
+</div>
