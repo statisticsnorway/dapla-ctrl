869f8fd8b984dec2bf6260296f95c6d085c7456a
Are Stenbrenden <2938694+nomx3@users.noreply.github.com>
2026-09-07 13:58:24 +0200

    fix: messages status check constraint (#99)
    * fix: messages status check constraint

---
+++ api/internal/database/migrations/0017_alter_messages_status_contraint.sql
@@ -0,0 +1,9 @@
+-- +goose Up
+ALTER TABLE messages
+DROP CONSTRAINT IF EXISTS messages_status_check
+;
+
+ALTER TABLE messages
+ADD CONSTRAINT messages_status_check
+CHECK (status IN ('PENDING', 'PUBLISHED', 'SUCCESSFUL', 'UNSUCCESSFUL'))
+;
