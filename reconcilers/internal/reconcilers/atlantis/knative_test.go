160bba5e0c3581098c40814af57b20a70b4e887f
nicolst <35232838+nicolst@users.noreply.github.com>
2026-09-16 13:17:58 +0200

    test: more cases for atlantis

--- reconcilers/internal/reconcilers/atlantis/knative_test.go
+++ reconcilers/internal/reconcilers/atlantis/knative_test.go
@@ -9,6 +9,7 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"knative.dev/pkg/kmp"
 	"knative.dev/serving/pkg/client/clientset/versioned/fake"
 )
 
@@ -94,4 +95,64 @@ func TestReconcileKnativeService(t *testing.T) {
 
 	})
 
+	t.Run("image updated on config change", func(t *testing.T) {
+		r.atlantisImage = "atlantis:v1"
+
+		if err := r.reconcileKnativeService(t.Context(), atlantisName, namespace, repoAllowList); err != nil {
+			t.Fatal(err)
+		}
+
+		knsvc, err := fakeServing.Services(namespace).Get(t.Context(), atlantisName, metav1.GetOptions{})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		container := knsvc.Spec.Template.Spec.Containers[0]
+
+		if container.Image != r.atlantisImage {
+			t.Errorf("incorrect image %q, expected %q", container.Image, r.atlantisImage)
+		}
+	})
+
+	t.Run("service is changed back if live version has been altered", func(t *testing.T) {
+		knsvc, err := fakeServing.Services(namespace).Get(t.Context(), atlantisName, metav1.GetOptions{})
+		if err != nil {
+			t.Fatal(err)
+		}
+		original := knsvc.DeepCopy()
+
+		container := &(knsvc.Spec.Template.Spec.Containers[0])
+		container.Image = "atlantis:my-dev-version"
+		container.Env = nil
+
+		if _, err := fakeServing.Services(namespace).Update(t.Context(), knsvc, metav1.UpdateOptions{}); err != nil {
+			t.Fatal(err)
+		}
+
+		if err := r.reconcileKnativeService(t.Context(), atlantisName, namespace, repoAllowList); err != nil {
+			t.Fatal(err)
+		}
+
+		knsvc, err = fakeServing.Services(namespace).Get(t.Context(), atlantisName, metav1.GetOptions{})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if diff, err := kmp.SafeDiff(knsvc.Spec.ConfigurationSpec, original.Spec.ConfigurationSpec); err != nil {
+			t.Fatal(err)
+		} else if diff != "" {
+			t.Fatalf("diff between original and reconciled altered version: %v", diff)
+		}
+
+	})
+
+	t.Run("won't run if missing template", func(t *testing.T) {
+		r.knativeServiceTemplate = nil
+
+		if err := r.reconcileKnativeService(t.Context(), atlantisName, namespace, repoAllowList); err == nil {
+			t.Fatal("knative reconciler ran with nil template")
+		} else if !strings.Contains(err.Error(), "missing knative template") {
+			t.Fatalf("unknown error occured: %s", err)
+		}
+	})
 }
